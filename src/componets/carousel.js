1107bfe6e85e7dc54d745f4f8665a42f48eeec4a
Christian Gonzalez <christian.gonzalez633@t-mobile.com>
2024-10-31 14:04:24 -0500

    create baseline carousel

--- src/componets/carousel.js
+++ src/componets/carousel.js
@@ -14,16 +14,27 @@ export const Carousel = {
     this.carousel = new Swiper(el, {
       loop: true,
       // spaceBetween: 24,
+      navigation: {
+        enabled: false,
+        nextEl: 'swiper-button-next2',
+        prevEl: 'swiper-button-prev2',
+      },
       allowTouchMove: true,
       slidesPerView: 1,
       breakpoints: {
         320: {
           slidesPerView: 1,
         },
+        576: {
+          slidesPerView: 1,
+        },
         768: {
           slidesPerView: 2,
         },
         1024: {
+          slidesPerView: 2,
+        },
+        1200: {
           slidesPerView: 3,
         }
       },
@@ -46,7 +57,6 @@ export const Carousel = {
     })
   }
 }
-
 
 // export const Carousel2 = {
 //   init() {
